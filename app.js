d6dbdc30bb530a698137d5f03387b4a0ee017571
mahmudnibir <nibirbbkr@gmail.com>
2024-06-18 19:23:05 +0600

    Update app.js

--- app.js
+++ app.js
@@ -42,4 +42,24 @@ document.addEventListener('DOMContentLoaded', function() {
             location.reload();
         });
     }
+
+    // Contact form functionality
+    const inputs = document.querySelectorAll(".input");
+
+    function focusFunc() {
+        let parent = this.parentNode;
+        parent.classList.add("focus");
+    }
+
+    function blurFunc() {
+        let parent = this.parentNode;
+        if (this.value == "") {
+            parent.classList.remove("focus");
+        }
+    }
+
+    inputs.forEach((input) => {
+        input.addEventListener("focus", focusFunc);
+        input.addEventListener("blur", blurFunc);
+    });
 });
